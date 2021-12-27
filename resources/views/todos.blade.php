daf6c4e2d6d2bdaf830321a98d336158abbc1a31
abdulsalam-create <you@example.com>
2021-12-27 20:36:51 +0100

    initial commit

---
+++ resources/views/todos.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+
+@section('content')
+
+<div class="row">
+    <div class=" justify-content-center mb-5">
+<form action="/create/todo" method="post">
+
+    {{ csrf_field() }}
+
+<input type="text" class="form-control input-lg" name="todo" placeholder="Create a new todo">
+
+</form>
+    </div>
+</div>
+@foreach ($todos as $todo)
+{{$todo->todo}} <a href="{{ route('todo.delete', ['id' => $todo->id]) }}" class="btn btn-danger"> x </a>
+<a href="{{ route('todo.update', ['id' => $todo->id]) }}" class="btn btn-primary btn-xs"> update </a>
+
+    @if(!$todo->completed)
+    <a href="{{ route('todos.completed', ['id' => $todo->id])  }}" class="btn btn-xs btn-success">Mark as completed</a>
+
+    @else
+
+    <span class="text-success">Completed!</span>
+
+    @endif
+
+<hr>
+@endforeach;
+@stop
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+
+
+
+    <hr>
+    <a href="/todos">Visit my todos</a>
+@stop
